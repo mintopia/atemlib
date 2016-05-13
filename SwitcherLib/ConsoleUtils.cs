8fc528c2ccf0d79f15a0cfaff8f6d721901d4fff
Jessica Smith <me@murray-mint.co.uk>
2016-05-13 11:59:56 +0100

    Update ConsoleUtils.cs

--- SwitcherLib/ConsoleUtils.cs
+++ SwitcherLib/ConsoleUtils.cs
@@ -14,7 +14,7 @@ namespace SwitcherLib
             Version version = Assembly.GetEntryAssembly().GetName().Version;
             AssemblyTitleAttribute title = (AssemblyTitleAttribute)Assembly.GetEntryAssembly().GetCustomAttribute(typeof(AssemblyTitleAttribute));
             Console.Out.WriteLine(String.Format("{0} {1}.{2}.{3}", title.Title, version.Major.ToString(), version.Minor.ToString(), version.Revision.ToString()));
-            Console.Out.WriteLine("Michael Smith <me@murray-mint.co.uk>");
+            Console.Out.WriteLine("Jessica Smith <jess@mintopia.net>");
             Console.Out.WriteLine("This software is released under the MIT License");
         }
     }
