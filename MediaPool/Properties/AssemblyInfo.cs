239cbe00fa34f46f613af47fea1e187f73122501
Jessica Smith <jess@mintopia.net>
2018-02-01 23:17:17 +0000

    Test against ATEM library 7.3 Update versioning and about info.

--- MediaPool/Properties/AssemblyInfo.cs
+++ MediaPool/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ATEM Media Pool List")]
 [assembly: AssemblyDescription("Retrieves a list of media in the media pool of an ATEM Switcher")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Michael Smith")]
+[assembly: AssemblyCompany("Jessica Smith")]
 [assembly: AssemblyProduct("ATEM Switcher Library")]
-[assembly: AssemblyCopyright("© Copyright Michael Smith, 2014")]
+[assembly: AssemblyCopyright("© Copyright Jessica Smith, 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
--- Prep Release.cmd
+++ Prep Release.cmd
@@ -4,4 +4,5 @@ xcopy MediaPool\bin\release\MediaPool.exe Release /C /H /Y
 xcopy MediaPool\bin\release\*.dll Release /C /H /Y
 xcopy MediaUpload\bin\release\MediaUpload.exe Release /C /H /Y
 xcopy MediaUpload\bin\release\*.dll Release /C /H /Y
-cp readme.txt Release/readme.txt
+xcopy readme.md Release\readme.md /C /H /Y
+pause
